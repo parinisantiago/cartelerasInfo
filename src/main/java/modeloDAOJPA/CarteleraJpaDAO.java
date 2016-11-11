a6ac6440a08b5a6f298b3045e380e4de1ac8f66f
parinisantiago <parinisantiago@gmail.com>
2016-11-11 18:17:15 -0300

    clase carteleraDao

---
+++ src/main/java/modeloDAOJPA/CarteleraJpaDAO.java
@@ -0,0 +1,31 @@
+package modeloDAOJPA;
+
+import java.util.List;
+
+import modelo.Cartelera;
+import modelo.Usuario;
+
+public class CarteleraJpaDAO extends JpaDao<Cartelera> implements CarteleraDAO {
+
+	
+	@Override
+	public List<Usuario> getInteresados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Usuario> getUsuariosEliminar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Usuario> getUsuariosPublicar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
