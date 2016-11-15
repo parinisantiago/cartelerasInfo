3cddfd0ecdcc96706e4e6642f654d0ea8007fcab
parinisantiago <parinisantiago@gmail.com>
2016-11-15 18:12:35 -0300

    cambios de agus

--- src/main/java/modeloDAOJPA/CarteleraJpaDAO.java
+++ src/main/java/modeloDAOJPA/CarteleraJpaDAO.java
@@ -9,6 +9,11 @@ import modeloDAO.CarteleraDAO;
 public class CarteleraJpaDAO extends JpaDao<Cartelera> implements CarteleraDAO {
 
 	
+	public CarteleraJpaDAO() {
+		super(Cartelera.class);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public List<Usuario> getInteresados() {
 		// TODO Auto-generated method stub
