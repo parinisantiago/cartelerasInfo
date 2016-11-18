148153da09ba7c671c266bcf5516665fdb83839d
Agustín <agustinlizarralde@gmail.com>
2016-11-17 21:14:22 -0300

    arreglado error de nombre de clase

--- src/main/java/modeloDAOJPA/DAOFactory.java
+++ src/main/java/modeloDAOJPA/DAOFactory.java
@@ -1,28 +1,64 @@
 package modeloDAOJPA;
 
+import modelo.Anuncio;
+import modelo.Cartelera;
+import modelo.Comentario;
+import modelo.Notificacion;
+import modelo.Rol;
+import modelo.Usuario;
+import modeloDAO.AnuncioDAO;
+import modeloDAO.CarteleraDAO;
+import modeloDAO.ComentarioDAO;
+import modeloDAO.NotificacionDAO;
+import modeloDAO.RolDAO;
+
 public class DAOFactory {
 	
-	public static AnuncioJpaDAO getAnuncioDao(){
+	public static AnuncioDAO getAnuncioDao(){
 		return new AnuncioJpaDAO();
 	}
 	
-	public static CarteleraJpaDAO getCarteleraDao(){
+	public static CarteleraDAO getCarteleraDao(){
 		return new CarteleraJpaDAO();
 	}
 	
-	public static ComentarioJpaDAO getComentarioDao(){
+	public static ComentarioDAO getComentarioDao(){
 		return new ComentarioJpaDAO();
 	}
 	
-	public static NotificacionJpaDAO getNotificacionDao(){
+	public static NotificacionDAO getNotificacionDao(){
 		return new NotificacionJpaDAO();
 	}
 	
-	public static RolJpaDAO getRolDao(){
+	public static RolDAO getRolDao(){
 		return new RolJpaDAO();
 	}
 	
 	public static UsuarioJpaDAO getUsuarioDao(){
+		return new UsuarioJpaDAO();
+	}
+	
+	public static AnuncioDAO getDao(Anuncio anuncio){
+		return new AnuncioJpaDAO();
+	}
+	
+	public static CarteleraDAO getDao(Cartelera cartelera){
+		return new CarteleraJpaDAO();
+	}
+	
+	public static ComentarioDAO getDao(Comentario comentario){
+		return new ComentarioJpaDAO();
+	}
+	
+	public static NotificacionDAO getDao(Notificacion notificacion){
+		return new NotificacionJpaDAO();
+	}
+	
+	public static RolDAO getDao(Rol rol){
+		return new RolJpaDAO();
+	}
+	
+	public static UsuarioJpaDAO getDao(Usuario usuario){
 		return new UsuarioJpaDAO();
 	}
 	
