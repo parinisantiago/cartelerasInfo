5ac74f66cd6ebc74af82fe5d661fbdc60add001c
parinisantiago <parinisantiago@gmail.com>
2017-01-28 18:37:24 -0300

    separo las directivas en un archivo aparte

--- src/main/webapp/js/cartelerasinfo.js
+++ src/main/webapp/js/cartelerasinfo.js
@@ -1,23 +1,6 @@
 angular.module("cartelerasInfo", [])
 
 
-angular.module("cartelerasInfo").directive("loginnav",function(){
-	return{
-		restrict:'E',
-		templateUrl:'templates/navbar/loginnav.html'
-	}
-});
-
-angular.module("cartelerasInfo").directive("logoutnav",function(){
-	return{
-		restrict:'E',
-		templateUrl:'templates/navbar/logoutnav.html'
-	}
-});
-
-
-
-
 
 
 
---
+++ src/main/webapp/js/directives/navbars.js
@@ -0,0 +1,15 @@
+angular.module("cartelerasInfo").directive("loginnav",function(){
+	return{
+		restrict:'E',
+		templateUrl:'templates/navbar/loginnav.html'
+	}
+});
+
+angular.module("cartelerasInfo").directive("logoutnav",function(){
+	return{
+		restrict:'E',
+		templateUrl:'templates/navbar/logoutnav.html'
+	}
+});
+
+
