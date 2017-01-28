979f49caacfc930fe44a23e9d4e3dbe2a6b99117
parinisantiago <parinisantiago@gmail.com>
2017-01-28 18:29:05 -0300

    saco la logica del modelo  la paso al controlador

--- src/main/webapp/js/controllers/login.js
+++ src/main/webapp/js/controllers/login.js
@@ -1,11 +1,28 @@
 angular.module("cartelerasInfo").controller("loginController",["$scope","$http", function($scope,$http){
+	
 	$scope.isUser = false;
+	$scope.errorMsg = false;
+	
+	$scope.isLogged = function(){
+		return $scope.isUser;
+	};
+	
+	$scope.isNotLogged = function(){
+		return !$scope.isUser;
+	};
+	
+	$scope.error = function(){
+		return $scope.errorMsg;
+	};
 	
 	$scope.validateLogin = function(login){
+		
+		$scope.errorMsg = false; //borra el mensaje de error.
+		
 		$http.post("REST/login",login).then(function(data){
 			$scope.isUser = true;
 		},function(data){
-			$scope.isUser = false;
+			$scope.errorMsg = true;
 		})
 	};
 }]);
