4fab399e594bd2f8d0a46b454465ae6721819e42
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-05-05 19:42:52 -0300

    al loguearse ahora se actualiza el nombre de usuario

--- src/main/webapp/app/controllers/loginController.js
+++ src/main/webapp/app/controllers/loginController.js
@@ -4,19 +4,20 @@ app.controller('loginCtl'
 		$scope.form={
 				user : '',
 				password: ''
-		};
-		$scope.usuario= userService.getUserData(); 
+		}; 
 		userService.loginRefresh();
 		$scope.loguear = function(){
 			userService.login($scope.form.user, $scope.form.password)
 		    .then(function(data){
 					$scope.form.password='';
+					$scope.usuario= userService.getUserData();
 				})
 			.catch(function(error){
 				$scope.form.password='';
 			})
 		};
 		$scope.isLogged = function(){
+			$scope.usuario= userService.getUserData();
 			return userService.isLogged();
 		};
 		$scope.logout = function(){
