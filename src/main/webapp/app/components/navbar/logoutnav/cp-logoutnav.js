77803fd6db8a9637e0a5c5540b45e2ef7d41b4ea
parinisantiago <parinisantiago@gmail.com>
2017-05-07 18:57:50 -0300

    wrapper de logout

--- src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
+++ src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
@@ -1,3 +1,14 @@
+app.controller('logoutnavController', logoutnavController);
+logoutnavController.$inject = ['userService', '$scope'];
+
+function logoutnavController(userService, $scope) {
+		$scope.logout = function(){
+			userService.logout();
+		}
+}
+
 app.component("logoutnav", {
+		controller: 'logoutnavController',
 		templateUrl: 'app/components/navbar/logoutnav/logoutnav.html',
+	
 });
