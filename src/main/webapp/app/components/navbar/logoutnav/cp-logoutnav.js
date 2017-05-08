70d895744df2ddb5108303b8ea09f554da49e2eb
parinisantiago <parinisantiago@gmail.com>
2017-05-07 21:00:42 -0300

    funciones para cambiar el body

--- src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
+++ src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
@@ -1,9 +1,15 @@
 app.controller('logoutnavController', logoutnavController);
-logoutnavController.$inject = ['userService', '$scope'];
+logoutnavController.$inject = ['userService', '$scope', 'localstorage'];
 
-function logoutnavController(userService, $scope) {
+function logoutnavController(userService, $scope, localstorage) {
 		$scope.logout = function(){
 			userService.logout();
+		}
+		$scope.publicaciones = function(){
+			localstorage.setSessionItem('body', 'publicaciones');
+		}
+		$scope.abmUsuario = function(){
+			localstorage.setSessionItem('body', 'abmUsuario');
 		}
 }
 
