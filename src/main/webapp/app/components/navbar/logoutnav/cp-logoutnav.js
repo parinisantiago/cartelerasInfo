b1d8405bb88adfe05c2a6238b4f6fabda48de83d
parinisantiago <parinisantiago@gmail.com>
2017-05-07 21:40:19 -0300

    nombre de usuario en la barra de navegacion

--- src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
+++ src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
@@ -2,8 +2,16 @@ app.controller('logoutnavController', logoutnavController);
 logoutnavController.$inject = ['userService', '$scope', 'localstorage'];
 
 function logoutnavController(userService, $scope, localstorage) {
+	
+		$scope.form={
+			user : '',
+			password: ''
+		}; 
+		$scope.form.password='';
+		$scope.usuario= userService.getUserData();
 		$scope.logout = function(){
 			userService.logout();
+			localstorage.setSessionItem('body', 'publicaciones');
 		}
 		$scope.publicaciones = function(){
 			localstorage.setSessionItem('body', 'publicaciones');
