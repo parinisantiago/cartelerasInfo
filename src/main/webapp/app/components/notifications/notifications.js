0131cfc8519263ed1a0f404ac34a5401861a04e7
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-05-10 20:09:22 -0300

    agregado sistema de notificaciones tag <notifications></notifications> mostrara las notificaciones de notificationService para agregar usar notificationService.addNotificacion(titulo, cuerpo, ['success', 'danger',etc] , si debe desaparecer[predef:true] , tiempo antes de desaparecer [predef:2 seg]);

---
+++ src/main/webapp/app/components/notifications/notifications.js
@@ -0,0 +1,57 @@
+notificationsController.$inject = ['notificationService','$timeout'];
+function notificationsController(notificationService,$timeout) {
+	var ctrl = this;
+	this.notificacion=null;
+	this.show = true;
+	
+	this.timeout = function(notificacion){
+		
+		var func = function(params){
+				if(angular.equals(params[0],params[1].notificacion)){
+			  		params[1].nextNotificacion();
+				}
+	  		};
+	  		console.log(notificacion.tiempo);
+  		$timeout(func, notificacion.tiempo, true, [notificacion, this]);
+	};
+	
+	this.nextNotificacion = function(){
+		notificationService.removeNotificacion(this.notificacion);
+		this.notificacion = null;
+		this.show = false;
+		var fnot = notificationService.getNotificaciones()[0];
+		if(fnot != null){
+  			this.addNotificacion(fnot);
+  		}
+	};
+	
+	this.addUpdate = function(notificacion){
+		this.addNotificacion(notificacion);
+	};
+	
+	this.removeUpdate = function(notificacion){
+		if(angular.equals(notificacion,ctrl.notificacionActual)){
+			this.notificacion = null;
+			this.show = false;
+		}
+	};
+	
+	this.addNotificacion = function(notif){
+		if(this.notificacion == null){
+			this.notificacion = notif;
+			this.show=true;
+			if(this.notificacion.desaparece){
+				console.log("crear timeout");
+				console.log(this.notificacion);
+				this.timeout(this.notificacion);
+			}
+		}
+	};
+	
+	notificationService.addObserver(this);
+}
+
+app.component('notifications', {
+  templateUrl: 'app/components/notifications/notifications.html',
+  controller: notificationsController,
+});
---
+++ src/main/webapp/app/services/notificationService.js
@@ -0,0 +1,55 @@
+app.factory("notificationService",
+		[
+		function(){
+		    
+			var notificacion = function notificacion(titulo, cuerpo, tipo, desaparece, tiempo ){
+				this.titulo = titulo;
+				this.cuerpo = cuerpo;
+				this.tipo = typeof tipo == 'string' || typeof tipo != 'undefined'? tipo : 'info';
+				this.desaparece = typeof desaparece == 'boolean' ? desaparece : true;
+				this.tiempo = typeof tiempo == 'number'? tiempo : 2000;
+			};
+		    
+			var observers = [];
+			
+			var notificaciones = [];
+			
+		    var interfazPublica = {
+		    	
+		    	getNotificaciones : function(){ return notificaciones; },
+		    	
+		    	addNotificacion : function(titulo, cuerpo, tipo, desaparece, tiempo){
+		    		var notif = new notificacion(titulo, cuerpo, tipo, desaparece, tiempo);
+		    		notificaciones.push(notif);
+		    		angular.forEach(observers, function(value, key) {
+		    			  if(value){ value.addUpdate(notif);}
+		    			  else{this.removeObserver(value);}
+		    			});
+		    	},
+		    	
+		    	removeNotificacion : function(notif){
+		    		notificaciones.splice(notificaciones.indexOf(notif),1);
+		    		angular.forEach(observers, function(value, key) {
+		    			  if(value){ value.removeUpdate(notif);}
+		    			  else{this.removeObserver(value);}
+		    			});
+		    	},
+		    	
+		    	addObserver : function(observer){
+		    		observers.push(observer);
+		    	},
+		    	
+		    	removeObserver : function(observer){
+		    		observers.split(observers.indexOf(observer),1);
+		    	},
+		    	
+		    	notify : function(){
+		    		angular.forEach(observers, function(value, key) {
+		    			  if(value){ value.update();}
+		    			  else{this.removeObserver(value);}
+		    			});
+		    	}
+		    }
+		    
+		    return interfazPublica;
+		}]);
