6b0584b68a46524e0ccfc3d3fad48bb1efb797ec
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-05-16 16:36:27 -0300

    Arreglado que los anuncios no se expandian ademas ahora lo hacen por angular

--- src/main/webapp/app/components/anuncio/anuncio.js
+++ src/main/webapp/app/components/anuncio/anuncio.js
@@ -2,6 +2,13 @@ app.controller('anuncioController', anuncioController);
 anuncioController.$inject = ['$scope', 'todopoderosoDAO', 'userService', '$http'];
 
 function anuncioController($scope, todopoderosoDAO, userService, $http) {
+	$scope.expandido=false;
+	$scope.expandir = function(){
+		$scope.expandido=true;
+	}
+	$scope.achicar = function(){
+		$scope.expandido=false;
+	}
 }
 
 app.component("anuncio", {
