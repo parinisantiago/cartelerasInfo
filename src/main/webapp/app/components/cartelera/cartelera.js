c0c0dbb48bca8df3fc199f779757121fd61fb7eb
parinisantiago <parinisantiago@gmail.com>
2017-06-28 09:47:42 -0300

    funcion de callback

--- src/main/webapp/app/components/cartelera/cartelera.js
+++ src/main/webapp/app/components/cartelera/cartelera.js
@@ -2,9 +2,28 @@ app.controller('carteleraController', carteleraController);
 carteleraController.$inject = ['$scope', 'todopoderosoDAO', 'userService', '$http'];
 
 function carteleraController($scope, todopoderosoDAO, userService, $http) {
+	var ctl = this
+	
+	ctl.deleteCartel = function(id, titulo){
+		if( confirm("Eliminar anuncio "+ titulo + "?") ){
+			todopoderosoDAO.eliminarAnuncio(id)
+			.then(function(data){
+				todopoderosoDAO.getCarteleraById(ctl.cart.id)
+				.then(function(data){
+					ctl.cart = data
+				})
+			})
+			.catch(function(error){
+				console.log("ocurrio un error, ups")
+				console.log(error);
+			})
+		}
+	}
+	
 	$scope.eliminar = function(){
 		return userService.getUserData().cartelerasEliminar
 	}
+
 }
 
 app.component("cartelera", {
