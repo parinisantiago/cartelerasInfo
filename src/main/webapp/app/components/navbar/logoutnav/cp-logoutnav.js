c76b19cb3cd889691cb63105bbef6959ce4aa3cf
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-07-12 19:32:11 -0300

    Cambiando los botones de navegacion para que usen el ruteo nuevo

--- src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
+++ src/main/webapp/app/components/navbar/logoutnav/cp-logoutnav.js
@@ -1,33 +1,28 @@
 app.controller('logoutnavController', logoutnavController);
-logoutnavController.$inject = ['userService', '$scope', 'localstorage'];
+logoutnavController.$inject = ['userService', '$scope', 'localstorage', '$state'];
 
-function logoutnavController(userService, $scope, localstorage) {
+function logoutnavController(userService, $scope, localstorage, $state) {
+	$scope.logueadoOHome = function(){
+		var logueado = userService.isLogged();
+		if(!logueado){
+			$state.go("home");
+		}
+		return logueado;
+	}
 	
-		$scope.form={
-			user : '',
-			password: ''
-		}; 
-		$scope.form.password='';
-		$scope.usuario= userService.getUserData();
-		$scope.logout = function(){
-			userService.logout();
-			localstorage.setSessionItem('body', 'publicaciones');
-		}
-		$scope.publicaciones = function(){
-			localstorage.setSessionItem('body', 'publicaciones');
-		}
-		$scope.abmUsuario = function(){
-			localstorage.setSessionItem('body', 'abmUsuario');
-		}
-		$scope.configuracion = function(){
-			localstorage.setSessionItem('body', 'configuracion');
-		}
-		$scope.misAnuncios = function(){
-			localstorage.setSessionItem('body', 'misAnuncios');
-		}
-		$scope.administrar = function(){
-			return (userService.getUserData().rol.id == '1')
-		}
+	$scope.form={
+		user : '',
+		password: ''
+	}; 
+	$scope.form.password='';
+	$scope.usuario= userService.getUserData();
+	$scope.logout = function(){
+		userService.logout();
+		$state.go('home');
+	}
+	$scope.administrar = function(){
+		return userService.isAdmin();
+	}
 }
 
 app.component("logoutnav", {
