20ef70a6d5799eed1a279cda45111db0e3eaa5fe
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-07-14 20:10:57 -0300

    agregado buscador en cartelera princiapl

--- src/main/webapp/app/components/listCartelera/listCartelera.js
+++ src/main/webapp/app/components/listCartelera/listCartelera.js
@@ -163,7 +163,7 @@ function listCarteleraController($scope, todopoderosoDAO, userService, notificat
 	}
 	
 	$scope.publicar = function(){
-		return ((userService.getUserData().rol.id == '2') && (userService.getUserData().cartelerasModificar.indexOf($scope.carteleraActiva.id) != -1));
+		return ((userService.getUserData().rol.id == '2') && $scope.carteleraActiva && (userService.getUserData().cartelerasModificar.indexOf($scope.carteleraActiva.id) != -1));
 	}
 	
 	$scope.removeInteres = function(){
