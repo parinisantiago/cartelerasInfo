981818ad50d93ac853369e126a2d41311a97baa7
AgustinLizarralde <agustinlizarralde@gmail.com>
2017-08-22 20:04:39 -0300

    al crear la estructura de carpetas para imagenes, tambien se descarga la imagen default

--- src/main/java/utilidades/FileManager.java
+++ src/main/java/utilidades/FileManager.java
@@ -3,13 +3,18 @@ package utilidades;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileManager {
-
-	private static final String defaultProfilePic = FileManager.ImagenesPerfilDir+"default.png";
 	
-	public static final String multimediaDir = "/home/agustin/workspace/cartelerasInfo/src/main/webapp/";
+	public static final String baseDir = Paths.get("").toAbsolutePath() + "/cartelerasInfo/";
+	
+	public static final String multimediaDir = baseDir + "multimedia/";
 	public static final String ImagenesDir = multimediaDir + "img/";
 	public static final String ImagenesAnuncioDir = ImagenesDir + "upload/";
 	public static final String ImagenesPerfilDir = ImagenesDir + "perfil/";
@@ -19,6 +24,54 @@ public class FileManager {
 	public static final String ImagenesPerfilDirURL = ImagenesDirURL + "perfil/";
 	
 	public static final String defaultProfilePicURL = ImagenesPerfilDirURL +"default.png";
+	private static final String defaultProfilePic = ImagenesPerfilDir+"default.png";
+	
+	private static boolean directoriosCreados = false;
+	
+	
+	private static void downloadDefaultPic(){
+		String urlString = "https://www.youngliving.com/vo/content/app/design/images/default-profile-image.jpg";
+		try{
+			InputStream in = new URL(urlString).openStream();
+		    Files.copy(in, Paths.get(FileManager.defaultProfilePic));
+		}
+		catch (Exception e) {
+			System.out.println("Error al descargar la imagen de perfil default");
+		}
+	}
+	
+	private static void createDirIfNotExist(File file){
+		if(!file.exists()){ 
+        	file.mkdir();
+        }
+	}
+	
+	private static void crearDirectorios(){
+		if(!directoriosCreados){
+			File file;
+			file = new File(baseDir);
+	        FileManager.createDirIfNotExist(file);
+	        
+	        file = new File(multimediaDir);
+	        FileManager.createDirIfNotExist(file);
+	        
+	        file = new File(ImagenesDir);
+	        FileManager.createDirIfNotExist(file);
+			
+	        file = new File(ImagenesAnuncioDir);
+	        FileManager.createDirIfNotExist(file);
+	        
+	        file = new File(ImagenesPerfilDir);
+	        FileManager.createDirIfNotExist(file);
+	        
+	        file = new File(defaultProfilePic);
+	        if(!file.exists()){
+	        	FileManager.downloadDefaultPic();
+	        }
+	        
+			directoriosCreados = true;
+		}
+	}
 	
 	public static String getDefaultName(MultipartFile fileInput){
         String name = fileInput.getOriginalFilename(); 
@@ -30,6 +83,10 @@ public class FileManager {
 	
 	public static String getExtension(String name){
 		return name.substring(name.lastIndexOf("."),name.length()).replaceAll("\\.", "");
+	}
+	
+	public FileManager(){
+		FileManager.crearDirectorios();
 	}
 	
 	public File getFile(String path){
@@ -49,6 +106,7 @@ public class FileManager {
 	}
 	
 	public String save(MultipartFile fileInput, String dirPath, String fileName) throws IOException{
+		
 		byte[] bytes = fileInput.getBytes(); 
 
         String rpath = dirPath ;
